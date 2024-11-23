e7de23579e26a509befdd27a9dd3e7ee45fc0d12
Icarus-dev <brain.m.config@gmail.com>
2024-11-23 22:19:21 +0200

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_supabase/api_keys.dart';
 import 'package:notes_supabase/auth_view.dart';
-import 'package:notes_supabase/notes_view.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: "https://jolgnbgjutnphtfzcpry.supabase.co",
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvbGduYmdqdXRucGh0ZnpjcHJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzOTAwMDcsImV4cCI6MjA0Nzk2NjAwN30.Ai_ox24bmw8Olp_loCvnp46DORWpclKglrSMuG1khO8",
+    url: ApiKeys.url,
+    anonKey: ApiKeys.apiKey,
   );
 
   runApp(const MyApp());
